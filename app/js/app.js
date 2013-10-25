d747e2e109e4d91c3fec8a5dca712841239d2c90
Kam Liu <KamChanLiu@Gmail.com>
2013-10-25 19:55:21 +0800

    Added simple marker for location.
    Fixed routing.

--- app/js/app.js
+++ app/js/app.js
@@ -3,6 +3,6 @@
 angular.module('taxistand', ['taxistandServices', 'google-maps']).
 	config(['$routeProvider', function($routeProvider) {
 		$routeProvider.
-			when('/taxis', {templateUrl: 'partials/location-list.html', controller: LocationListCtrl }).
-			otherwise({redirectTo: '/taxis'});
+			when('/', {templateUrl: 'partials/location-list.html', controller: LocationListCtrl }).
+			otherwise({redirectTo: '/'});
 	}]);
